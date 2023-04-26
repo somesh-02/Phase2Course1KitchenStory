1f8f33dc9d5263e563a8ed6805fb9c48b7e8f0f2
somesh <gannabattulasomesh@gmail.com>
2023-04-26 11:37:34 +0530

    first commit

---
+++ asd.js
@@ -0,0 +1,10 @@
+function testJS()
+{
+
+var price = document.getElementById('Price').value
+document.getElementById('hereprice').innerHTML = price;
+
+var name= document.getElementById('username').value
+document.getElementById('herename').innerHTML = name;
+
+}
